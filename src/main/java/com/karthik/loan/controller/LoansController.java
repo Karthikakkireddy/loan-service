d01a846d5e278ba6549c913c07ffca6e46b251ca
Karthikakkireddy <karthikakkireddy5@gmail.com>
2026-03-03 20:15:40 +0530

    Added create-loan API

---
+++ src/main/java/com/karthik/loan/controller/LoansController.java
@@ -0,0 +1,30 @@
+package com.karthik.loan.controller;
+
+import com.karthik.loan.constants.LoanConstants;
+import com.karthik.loan.dtos.LoansDto;
+import com.karthik.loan.dtos.ResponseDto;
+import com.karthik.loan.service.ILoansService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path= "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequiredArgsConstructor
+public class LoansController
+{
+
+    private final ILoansService iLoansService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createLoans(@RequestParam String mobileNumber)
+    {
+        iLoansService.createLoan(mobileNumber);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(LoanConstants.STATUS_201, LoanConstants.MESSAGE_201));
+    }
+}
--- src/main/java/com/karthik/loan/repository/LoansRepository.java
+++ src/main/java/com/karthik/loan/repository/LoansRepository.java
@@ -3,7 +3,9 @@ package com.karthik.loan.repository;
 import com.karthik.loan.entity.Loans;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LoansRepository extends JpaRepository<Loans, Long>
 {
-
+    Optional<Loans> findByMobileNumber(String mobileNumber);
 }
